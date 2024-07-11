c0aff6e24b1fe2a5f7df51fccab54a4493e60214
MatusValko <mvmatusvalko@gmail.com>
2024-07-11 18:07:47 +0200

    Added all core Gifts Room mechanics

--- Assets/Scripts/GiftBox.cs
+++ Assets/Scripts/GiftBox.cs
@@ -16,7 +16,8 @@ public class GiftBox : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _coinValue;
 
-
+    [SerializeField]
+    private GiftsRoomManager _giftsRoomManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,11 +32,37 @@ public class GiftBox : MonoBehaviour
 
     public void ClickOnBox()
     {
+        if (_giftsRoomManager.FreeSlotsToUnlock == 0)
+        {
+            //SHOW POP UP
+            _giftsRoomManager.ShowGetMoreSlotsForUnlock();
+            return;
+        }
+        _giftsRoomManager.FreeSlotsToUnlock -= 1;
+        _giftsRoomManager.UpdateFreeSlotsText();
+
+
+        _giftsRoomManager.GiftCounter += 1;
+
+        if (_giftsRoomManager.FreeSlotsToUnlock == 0)
+        {
+            _giftsRoomManager.ShowButtonsHideText();
+        }
+
+
+
         // GENERATE RANDOM NUMBER BETWEEN  1 AND 10
         Button button = gameObject.GetComponent<Button>();
         button.interactable = false;
         int randomNumber = UnityEngine.Random.Range(1, 11);
         _coinValue.text = randomNumber.ToString();
+
+        _giftsRoomManager.SetLastCoinValue(randomNumber);
+        _giftsRoomManager.SetLastCardText(_coinValue);
+
+        _giftsRoomManager.SwitchRooms();
+        _giftsRoomManager.SetRewardCoinText(randomNumber);
+        _giftsRoomManager.TotalCoinReward += randomNumber;
 
         // ADD VALUE TO TOTAL COINS OBTAINED DURING GIFTS ROOM 
         _giftBox.SetActive(false);
---
+++ Assets/Scripts/GiftsRoomManager.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+
+public class GiftsRoomManager : MonoBehaviour
+{
+    public const int MAXFreeSlotsToUnlock = 3;
+
+    public int FreeSlotsToUnlock = 3;
+
+    public int TotalCoinReward = 0;
+
+    [SerializeField]
+    private GameObject _buttons;
+    [SerializeField]
+    private GameObject _collectAllRewardsButton;
+    [SerializeField]
+    private GameObject _freeSlots;
+    [SerializeField]
+    private TextMeshProUGUI _freeSlotsText;
+    [SerializeField]
+    private GameObject _getMoreSlotsForUnlock;
+    [SerializeField]
+    private TextMeshProUGUI _rewardCoinText;
+
+    [SerializeField]
+    private GameObject _rewardRoom;
+
+    [SerializeField]
+    private GameObject _giftRoom;
+
+
+    [SerializeField]
+    private TextMeshProUGUI _lastCardText;
+    [SerializeField]
+    private int _lastCoinValue;
+
+    public int GiftCounter = 0;
+
+    void OnEnable()
+    {
+        FreeSlotsToUnlock = MAXFreeSlotsToUnlock;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowButtonsHideText()
+    {
+
+        if (FreeSlotsToUnlock == 0)
+        {
+            if (GiftCounter == 9)
+            {
+                _collectAllRewardsButton.SetActive(true);
+                _freeSlots.SetActive(false);
+                _buttons.SetActive(false);
+            }
+            else
+            {
+                _freeSlots.SetActive(false);
+                _buttons.SetActive(true);
+            }
+        }
+        else
+        {
+            _freeSlots.SetActive(true);
+            _buttons.SetActive(false);
+        }
+
+    }
+    public void UpdateFreeSlotsText()
+    {
+        _freeSlotsText.text = $"<color=yellow>{FreeSlotsToUnlock}</color>  free slots to unlock";
+    }
+
+    public void ShowGetMoreSlotsForUnlock()
+    {
+        _getMoreSlotsForUnlock.SetActive(true);
+        // Invoke("HideGetMoreSlotsForUnlock", 2);
+    }
+    public void SetRewardCoinText(int coinValue)
+    {
+        _rewardCoinText.text = coinValue.ToString() + "X";
+    }
+
+    public void SwitchRooms()
+    {
+        if (_rewardRoom.activeInHierarchy)
+        {
+            _rewardRoom.SetActive(false);
+            _giftRoom.SetActive(true);
+
+        }
+        else
+        {
+            _rewardRoom.SetActive(true);
+            _giftRoom.SetActive(false);
+        }
+    }
+    public void Claim2XCoins()
+    {
+        // SHOW ADVERT AND GIVE 2X COINS
+        _lastCoinValue = _lastCoinValue * 2;
+        _lastCardText.text = _lastCoinValue.ToString();
+        SwitchRooms();
+    }
+
+    public void Get3MoreSlots(int coinValue)
+    {
+        // SHOW ADVERT AND GIVE 3 MORE SLOT CHANCES
+        FreeSlotsToUnlock = MAXFreeSlotsToUnlock;
+        UpdateFreeSlotsText();
+        ShowButtonsHideText();
+        // SwitchRooms();
+    }
+    public void Claim(int coinValue)
+    {
+        SwitchRooms();
+        // ADD COINS
+    }
+    public void NoThanks()
+    {
+
+        gameObject.SetActive(false);
+        // SwitchRooms();
+        // ADD COINS
+    }
+
+    public void SetLastCardText(TextMeshProUGUI text)
+    {
+        _lastCardText = text;
+    }
+    public void SetLastCoinValue(int value)
+    {
+        _lastCoinValue = value;
+    }
+    private void CollectAllRewards()
+    {
+
+    }
+}
